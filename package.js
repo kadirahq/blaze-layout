b06e5aa12ce7bb813c14dc30c3f8d0f414cf8321
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-05-07 17:27:24 +0530

    Bump versions: 1.2.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Layout Manager for Meteor (designed for flow)',
-  version: '1.1.1',
+  version: '1.2.0',
   git: 'https://github.com/meteorhacks/flow-layout',
   name: "meteorhacks:flow-layout"
 });
